ee27ea3640175718bb1f31cae64f17e4648242a2
agilestarskim <agilestarskim@gmail.com>
2025-02-09 21:49:21 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Oing_LandingPage`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
